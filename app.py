5179f7c1da1562b79a6ad8604c8402373db0629d
Devissam6 <54173427+Devissam6@users.noreply.github.com>
2023-03-16 19:06:45 +0000

    more error handling, support for error messages, add ability to delete plans

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.secret_key = 'b1bb09fe442fc7569ff747c5544fec41ca220dc3f80e82d7fd4b6425cbd8dc
 
 DATABASE = 'foodData.db'
 
+# Simple function to query the database (adapted from Flask documentation)
 def get_db(query, args=(), one=False):
     db = sqlite3.connect(DATABASE)
     cur = db.cursor()
@@ -19,43 +20,33 @@ def get_db(query, args=(), one=False):
 
     return (res[0] if res else None) if one else res
 
+# Pull plan and food detail from database to be used in templates
 def get_plans():
     return get_db("SELECT * FROM plans")
 
 def get_foods():
     return get_db("SELECT * FROM foods")
 
-@app.route("/reset", methods=["GET", "POST"])
-def reset():
-    keylist = list(session.keys())
-    for key in keylist:
-        session.pop(key)
-    return redirect("/")
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
-
+    return redirect("/plan")
 
 @app.route("/plan", methods=["GET", "POST"])
 def plan():
-    # Response to POST request (user submitted input)
+    # Response to POST request (user submitted plan constraints by clicking "Preview" button)
     if request.method == "POST":
         # Gather info from form
         info = {"targetCal": request.form.get("targetCal", -1, float), 
                 "proteinMass": request.form.get("proteinMass", -1, float),
                 "fatPercent": request.form.get("fatPercent", -1, float)}
         # Return error for invalid input
-        errorMsg = "Invalid input for: "
-        error = False
-        for key, value in info.items():
-            if (value < 0 or
-                (key == "proteinMass" and value > (1/4) * info["targetCal"]) or
-                (key == "fatPercent" and ((value/100) * info["targetCal"] > info["targetCal"] - 4 * info["proteinMass"] or value > 100) and "proteinMass" not in errorMsg)):
-                errorMsg += ("({error}) ".format(error=key))
-                error = True
-        if error:
-            return render_template("plan.html", errorMsg=errorMsg, plansList=get_plans(), foodList=get_foods())
+        if (info["targetCal"] < 0 or
+            (info["proteinMass"] < 0 or info["proteinMass"] > (1/4) * info["targetCal"]) or
+            (info["fatPercent"] < 0 or ((info["fatPercent"]/100) * info["targetCal"] > info["targetCal"] - 4 * info["proteinMass"]) or info["fatPercent"] > 100)):
+            session["errorMsg"] = "Constraints specified invalid. Please try again."
+            return redirect("/plan")
+
         # Calculate values to fill table
         info["proteinEnergy"] = info["proteinMass"] * 4
         info["fatEnergy"] = info["targetCal"] * (info["fatPercent"]/100)
@@ -69,7 +60,16 @@ def plan():
     else:
         # Retrieve all plans from database
         if "plan_id" not in session:
-            return render_template("plan.html", plansList=get_plans())
+            if "errorMsg" in session:
+                errorMsg = session["errorMsg"]
+                session.pop("errorMsg")
+                return render_template("plan.html", plansList=get_plans(), errorMsg=errorMsg)    
+            elif "successMsg" in session:
+                successMsg = session["successMsg"]
+                session.pop("successMsg")
+                return render_template("plan.html", plansList=get_plans(), successMsg=successMsg)    
+            else:
+                return render_template("plan.html", plansList=get_plans())
         else:
             res = get_db("SELECT * FROM plans WHERE plan_id = ?", (session["plan_id"]), one=True)
 
@@ -89,7 +89,7 @@ def plan():
                             FROM foods AS f, plan_food_link AS pfl 
                             WHERE f.food_id = pfl.food_id AND pfl.plan_id = ?""", (session["plan_id"],))
             
-            foodInfoTotal = {"energy": 0, "fat": 0, "saturated_fat": 0, "carbohydrates": 0, "sugar": 0, "fibre": 0, "protein": 0, "salt": 0, "price": 0}
+            foodInfoTotal = {"energy": 0, "fat": 0, "saturated_fat": 0, "carbohydrates": 0, "sugar": 0, "fibre": 0, "protein": 0, "salt": 0, "price": 0, "mass": 0}
             for food in foodInfo:
                 foodInfoTotal["energy"] += food["energy"] * food["mass"] / 100
                 foodInfoTotal["fat"] += food["fat"] * food["mass"] / 100
@@ -100,8 +100,18 @@ def plan():
                 foodInfoTotal["protein"] += food["protein"] * food["mass"] / 100
                 foodInfoTotal["salt"] += food["salt"] * food["mass"] / 100
                 foodInfoTotal["price"] += food["price_per_kg"] * food["mass"] / 1000
+                foodInfoTotal["mass"] += food["mass"]
 
-            return render_template("plan.html", info=info, plansList=get_plans(), foodList=get_foods(), foodInfo=foodInfo, foodInfoTotal=foodInfoTotal)
+            if "errorMsg" in session:
+                errorMsg = session["errorMsg"]
+                session.pop("errorMsg")
+                return render_template("plan.html", info=info, plansList=get_plans(), foodList=get_foods(), foodInfo=foodInfo, foodInfoTotal=foodInfoTotal, errorMsg=errorMsg)
+            elif "successMsg" in session:
+                successMsg = session["successMsg"]
+                session.pop("successMsg")
+                return render_template("plan.html", info=info, plansList=get_plans(), foodList=get_foods(), foodInfo=foodInfo, foodInfoTotal=foodInfoTotal, successMsg=successMsg)
+            else:
+                return render_template("plan.html", info=info, plansList=get_plans(), foodList=get_foods(), foodInfo=foodInfo, foodInfoTotal=foodInfoTotal)
 
 
 @app.route("/saveplan", methods=["GET", "POST"])
@@ -158,9 +168,24 @@ def saveplan():
 @app.route("/loadplan", methods=["GET", "POST"])
 def loadplan():
     if request.method == "POST":
-        session["plan_id"] = request.form.get("loadPlanName", None, str)
+        # Get user's action input
+        action = request.form.get("planLoadDelete", None, str)
+        plan_id = request.form.get("loadPlanName", None, str)
+        # Action if user chose to load plan
+        if action == "load":
+            if plan_id != None:
+                session["plan_id"] = plan_id
+            return redirect("/plan")
+        # Action if user chose to delete plan
+        elif action == "delete":
+            if plan_id != None:
+                get_db("DELETE FROM plans WHERE plan_id = ?", (plan_id,))
+                get_db("DELETE FROM plan_food_link WHERE plan_id = ?", (plan_id,))
+                session.pop("plan_id")
+            return redirect("/plan")
+        else:
+            return redirect("/plan")
 
-        return redirect("/plan")
     else:
         return redirect("/plan")
 
@@ -229,6 +254,8 @@ def deletefood():
 def addfoodtoplan():
     if request.method == "POST":
         foodToAdd = request.form.get("foodToAdd", None, str)
+        if foodToAdd == None:
+            return redirect("/plan")
         get_db("""INSERT INTO plan_food_link (plan_id,
                                                 food_id,
                                                 mass)
